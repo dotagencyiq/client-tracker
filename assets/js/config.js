3db7b1a16a6e3cffe0a04b5e49a1fff2ff6b8666
dotagencyiq <evolutiontodaysecond@gmail.com>
2026-07-15 18:08:13 +0300

    commit

--- assets/js/config.js
+++ assets/js/config.js
@@ -25,7 +25,7 @@ const CLIENTS_DATABASE = {
     "client004": {
         name: "Khalied Agency",
         service: "Moteeion Graphic Video",
-        activeStep: 4,
+        activeStep: 3,
         isFinished: true     // ستظهر له رسالة النهاية تلقائياً
     }
     
